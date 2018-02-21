290b65cc3a2c0bb9d6545cc2d205a7bb0ba1bf65
mihou <2856416722@qq.com>
2018-02-21 09:27:26 +0800

    uu

--- soundcloud.js
+++ soundcloud.js
@@ -1,51 +1,51 @@
 const request = require("sync-request");
 
-const COOKIE_STRING = process.env.npm_config_cookie;
-const GET_VIDEO = "https://www.youlikehits.com/soundcloudplays.php?step=reload&rand=0.9043193618257324";
+const COOKIE_STRING =  '__utma=255682432.191511692.1519127802.1519137679.1519174825.3; __utmz=255682432.1519127802.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _pk_id.1.4ef9=9418a2a8be749d70.1519127804.4.1519174920.1519138772.; LBSESSIONID=A|WozKb|WozEo; PHPSESSID=57b1bb4e4de91f0b93c3f9369eb70938; _pk_cvar.1.4ef9=%7B%221%22%3A%5B%22userid%22%2C%222091938%22%5D%7D; _pk_ses.1.4ef9=*; __utmb=255682432.3.10.1519174825; __utmc=255682432; __utmt=1';
+const GET_VIDEO = "https://www.youlikehits.com/soundcloudplays.php?step=reload&rand="+Math.random();
 const USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
 const seconds = 3;
 const options = {
     headers : {
-    	'Cookie': COOKIE_STRING,
-    	'User-Agent' : USER_AGENT
-	}
+        'Cookie': COOKIE_STRING,
+        'User-Agent' : USER_AGENT
+    }
 };
 const NO_SONGS = "There are no more songs to play for points. Check back later";
 let count = 0;
 while(count < 50) {
-	count++;
+    count++;
 
-	let response = request('GET', GET_VIDEO, options);
-	response = response.getBody('utf8');
-	console.log(response);
-	if (response.includes(NO_SONGS)) {
-		process.exit(0);
-	}
-	let detailsStart = response.indexOf("imageWin") + "imageWin(".length;
-	let detailsEnd = response.indexOf(");", detailsStart);
-	let details = response.substring(detailsStart, detailsEnd);
-	details = details.split(",").map(function(element) {
-		return element.trim().replace(/'/g, "");
-	})
-	console.log(details);
-	if(details.length !== 4) {
-		continue;
-		process.exit(0);
-	}
+    let response = request('GET', GET_VIDEO, options);
+    response = response.getBody('utf8');
+    console.log(response);
+    if (response.includes(NO_SONGS)) {
+        process.exit(0);
+    }
+    let detailsStart = response.indexOf("imageWin") + "imageWin(".length;
+    let detailsEnd = response.indexOf(");", detailsStart);
+    let details = response.substring(detailsStart, detailsEnd);
+    details = details.split(",").map(function(element) {
+        return element.trim().replace(/'/g, "");
+    })
+    console.log(details);
+    if(details.length !== 4) {
+        continue;
+        process.exit(0);
+    }
 
-	var waitTill = new Date(new Date().getTime() + seconds * 1000);
-	while(waitTill > new Date()){}
+    var waitTill = new Date(new Date().getTime() + seconds * 1000);
+    while(waitTill > new Date()){}
 
-	const GET_POINTS = `https://www.youlikehits.com/soundcloudplaysplay.php?id=${details[0]}&step=points&x=${details[3]}&rand=0.04596779850586352`;
-	console.log(GET_POINTS);
-	try {
-		response = request('GET', GET_POINTS, options);
-	} catch(e) {
-		console.log(e);
-		continue;
-	}
-	response = response.getBody('utf8');
-	console.log(response);
-	console.log(count);
+    const GET_POINTS = `https://www.youlikehits.com/soundcloudplaysplay.php?id=${details[0]}&step=points&x=${details[3]}&rand=`+Math.random();
+    console.log(GET_POINTS);
+    try {
+        response = request('GET', GET_POINTS, options);
+    } catch(e) {
+        console.log(e);
+        continue;
+    }
+    response = response.getBody('utf8');
+    console.log(response);
+    console.log(count);
 }
 
