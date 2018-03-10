5967672bac099ac732c12ed1278db121a14e5f4e
mihou <2856416722@qq.com>
2018-03-10 18:46:38 +0800

    up

--- soundcloud.js
+++ soundcloud.js
@@ -1,6 +1,6 @@
 const request = require("sync-request");
 
-const COOKIE_STRING =  'tfuser=yzllz002; tfsecure=9d197bd21dc68b611d85aedff2338b41; tfpass=388d5a80de4fd3f269d6fab440892e0b; __utma=255682432.1129091615.1519126798.1520652638.1520661776.107; __utmz=255682432.1519126798.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _pk_id.1.4ef9=d457635c337fd395.1519126798.99.1520661814.1520653344.; LBSESSIONID=A|WqO2u|WqO2s';
+const COOKIE_STRING =  'tfuser=yzllz002; tfsecure=9d197bd21dc68b611d85aedff2338b41; tfpass=388d5a80de4fd3f269d6fab440892e0b; PHPSESSID=56989208f1acb0380c20dc5e97c64027; __utmt=1; __utma=255682432.1129091615.1519126798.1520661776.1520678588.108; __utmb=255682432.6.10.1520678588; __utmc=255682432; __utmz=255682432.1519126798.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); LBSESSIONID=A|WqO3Q|WqO2s; _pk_cvar.1.4ef9=%7B%221%22%3A%5B%22userid%22%2C%222091907%22%5D%7D; _pk_id.1.4ef9=d457635c337fd395.1519126798.100.1520678719.1520661814.; _pk_ses.1.4ef9=*';
 const GET_VIDEO = "https://www.youlikehits.com/soundcloudplays.php?step=reload&rand="+Math.random();
 const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0";
 const seconds = 3;
